c414ee0f9ef7942aa5eee07e667913f5cc44dde4
Antony Chen <tchen@digital313.com>
2014-08-16 16:44:59 -0400

    Added comments to makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -3,21 +3,36 @@
 
 ## Write a short comment describing this function
 
+# makeCacheMatrix - creates a list with setter and getter functions for
+# storing a matrix and its inverse solution
+# get / getInverse - get original matrix / get inverse solution (if stored)
+# set / setInverse - set original matrix / get original matrix (if stored)
+
 makeCacheMatrix <- function(x = matrix()) {
+  # initializing a new matrix, inverse hasn't been calculated yet so
+  # setting to NULL
   inv_matrix <- NULL
+  # initialize makeCacheMatrix with matrix passed to it as argument
+  # (same as calling makeCacheMatrix and then doing a $set)
+  stored_matrix <- x
+  # set - set intial matrix, means inverse is deleted
   set <- function(y = matrix()) {
     stored_matrix <<- y
     inv_matrix <<- NULL
   }
+  # get - returns initial matrix
   get <- function() {
     stored_matrix
   }
+  # setInverse - sets inverse matrix
   setInverse <- function(y = matrix()) {
     inv_matrix <<- y
   }
+  # getInverse - gets inverse matrix if stored, otherwise NULL
   getInverse <- function() {
     inv_matrix
   }
+  # expose functions so they can be called externally
   list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
